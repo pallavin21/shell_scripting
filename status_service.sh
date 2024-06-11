58f0375d74d63972c4ee1f4ada6e1f470e348db7
Ubuntu <ubuntu@ip-172-31-21-95.ec2.internal>
2024-06-11 17:56:02 +0000

    newchange

--- status_service.sh
+++ status_service.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+a=apache2
 
-a="apache2"
-sudo systemctl is-active $a
+sudo systemctl daemon-reload
+
 if
-        sudo systemctl status $a = active 
+        sudo systemctl is-active $a
 then
-	echo "service is active : $a"
+        echo "service is active : $a"
 
 else
 
- 	echo "service is inactive : $a"
+        echo "service is inactive : $a, starting $a"
+        sudo systemctl start $a
+        echo "$a service is started"
 fi
 
 
